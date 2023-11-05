efe7fae5e8844e9ae2d86e0c98f60defe2896a4f
flyinggip <flyinggip@blank.org>
2023-11-05 10:01:49 -0800

    Added: 2 examples in scratch: 1) the first tiny model; 2) a hook example

---
+++ scratch/tiny_model.py
@@ -0,0 +1,38 @@
+import torch
+
+class TinyModel(torch.nn.Module):
+
+    def __init__(self):
+        super(TinyModel, self).__init__()
+
+        self.linear1 = torch.nn.Linear(100, 200)
+        self.activation = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(200, 10)
+        self.softmax = torch.nn.Softmax()
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        x = self.softmax(x)
+        return x
+
+tinymodel = TinyModel()
+
+print('The model:')
+print(tinymodel)
+
+print('\n\nJust one layer:')
+print(tinymodel.linear2)
+
+print('\n\nModel params:')
+for param in tinymodel.parameters():
+    print(param.size())
+
+print('\n\nlinear2 params:')
+for param in tinymodel.linear2.parameters():
+    print(param.size())
+
+print('\n\nactivation params:')
+for param in tinymodel.activation.parameters():
+    print(param.size())
